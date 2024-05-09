69081f7cadbcfcb1eab40884549daca1db2e7105
imjiav <166671000+imjiav@users.noreply.github.com>
2024-05-09 09:11:17 +0800

    Update index.py
    00

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from api.openai import OpenAI
+from api.chatgpt import ChatGPT
 
 import os
 
