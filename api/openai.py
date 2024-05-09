58ca0cc990c703f636dca8e6f9c9bed61db4de20
imjiav <166671000+imjiav@users.noreply.github.com>
2024-05-09 08:46:25 +0800

    Update openai.py
    NIMAMQAMAMAMAMA

--- api/openai.py
+++ api/openai.py
@@ -12,7 +12,7 @@ line_bot_api = LineBotApi("LINE_CHANNEL_ACCESS_TOKEN")
 handler = WebhookHandler("LINE_CHANNEL_SECRET")
 
 # 設置OpenAI API 密鑰
-openai.api_key ="sk-proj-JAvUukDHjjcIG37TaF0VT3BlbkFJ4zLBwB3JBS6YZcAJubCO"
+openai.api_key ="OPEN_AI_KEY"
 
 # 與GPT模型交互的函數
 def chat_with_gpt(prompt):
